482bdff00e58c687f13868e91f8120d5de2c2295
Abusolay <sefat01625@gmail.com>
2025-05-01 00:33:27 +0600

    Dragon news haader logo section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from"../../assets/logo.png"
+import { format } from 'date-fns';
+// import { format, compareAsc } from "date-fns";
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center mt-10 gap-4'>
-            <img className='w-[350px]' src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
+            <img className='w-[400px]' src={logo} alt="" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-semibolt text-accent'>{format(new Date(), "EEEE, MMMM MM , yyyy")}</p>
         </div>
     );
 };
