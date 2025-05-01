69daa10361c7c015bce9bb27aae9fb17b5c55452
Abusolay <sefat01625@gmail.com>
2025-05-01 22:02:03 +0600

    Dragon news Q-Zone

--- src/Components/HomeLayout/RighAside/RighAside.jsx
+++ src/Components/HomeLayout/RighAside/RighAside.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import SocialLogin from '../../SocialLogin/SocialLogin';
+import FindUs from '../../FindUs/FindUs';
+import QZone from '../../QZone/QZone';
 
 const RighAside = () => {
     return (
-        <div>
+        <div className='space-y-8'>
             <SocialLogin></SocialLogin>
+            <FindUs></FindUs>
+            <QZone></QZone>
         </div>
     );
 };
