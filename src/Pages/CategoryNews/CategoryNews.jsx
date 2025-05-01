1549b351dfacac5bf42c177a06a5a6a35581ce93
Abusolay <sefat01625@gmail.com>
2025-05-01 14:48:49 +0600

    Dragon news left categories newa

--- src/Pages/CategoryNews/CategoryNews.jsx
+++ src/Pages/CategoryNews/CategoryNews.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-    const {id} = useParams();
-    console.log(id)
-    return (
-        <div>
-            CategoryNews -{id}
-        </div>
-    );
+  const { id } = useParams();
+  const data = useLoaderData();
+  // console.log(id, data)
+
+  const [CategoryNews, setCategoryNews] = useState([]);
+
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+      return;
+    } else if (id == "1") {
+      const filtereNews = data.filter(
+        (news) => news.others.is_today_pick == true
+      );
+      setCategoryNews(filtereNews)
+    }
+    else{
+        const filtereNews = data.filter((news) => news.category_id == id);
+        console.log(filtereNews);
+        setCategoryNews(filtereNews);
+    }
+    
+   
+  }, [data, id]);
+  return <div>Total {CategoryNews.length} news Found</div>;
 };
 
 export default CategoryNews;
